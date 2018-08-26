623c06ec2dc364b9315577c714c2dbba0ece1e39
Yohei Matz <coolism.productions@gmail.com>
2018-08-27 04:06:55 +0900

    Devリリース# Daily/Weeklyレポート用バッチ完成::Windowサイズ調整3

--- library/acm_auto_approve_by_selenium.py
+++ library/acm_auto_approve_by_selenium.py
@@ -62,7 +62,7 @@ def main():
     # ChromeのWebDriverオブジェクトを作成する。
     driver = webdriver.Chrome(chrome_options=options)
     # ウィンドウサイズを調整
-    driver.set_window_size(840,1680)
+    driver.set_window_size(840,1280)
 
     try:
         print('Debug1: O365ログイン画面への遷移')
